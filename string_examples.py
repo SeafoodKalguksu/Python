5eb8fd4045ac458af44c16ff21170b01f246b68b
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-05 01:44:05 +0900

    + find() and index()

--- string_examples.py
+++ string_examples.py
@@ -9,7 +9,6 @@ WORKING_DAYS = 'Mon Tue Wed Thu Fri'
 NAME = "Heeseok"
 INTEGERS = '123456789012345'
 
-
 def string_into_list() -> None:
     # Split a string into a list.
     # Setting the maxsplit parameter to 3, will return a list with 4.
@@ -25,16 +24,34 @@ def string_into_list() -> None:
     name: List = list(NAME)
     print(f"name = {name}")
 
-
 def list_to_string() -> None:
     # Convert a list to a string.
     capital_name: str = ''.join('HEESEOK')
     print(f"capital_name = {capital_name}")
 
+def find_examples() -> None:
+    # Return the lowest index in the string where substring sub is found.
+    # str.find(sub[, start[, end]])
+    index: int = INTEGERS.find('1', 3, 13)
+    print(f"INTEGERS.find('1', 3, 4), index = {index}")
+    index = INTEGERS.find('1')
+    print(f"INTEGERS.find('1'), index = {index}")
+    index = INTEGERS.find('11')
+    print(f"INTEGERS.find('11'), index = {index}")
+    index = INTEGERS.rfind('5')
+    print(f"INTEGERS.rfind('5'), index = {index}")
+
+def index_examples() -> None:
+    # Like find(), but raise ValueError when the substring is not found.
+    # str.index(sub[, start[, end]])
+    index = INTEGERS.index('1')
+    # index = integers.index('11')  # ValueError
+
 
 def main() -> None:
     string_into_list()
     list_to_string()
+    find_examples()
 
 if __name__ == "__main__":
     main()
