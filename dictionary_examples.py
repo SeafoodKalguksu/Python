dd25246d28c1f6ecada3652276b8eadb5c54e76a
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-29 23:33:33 +0900

    + dictionary

---
+++ dictionary_examples.py
@@ -0,0 +1,43 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+import collections
+from typing import Dict, DefaultDict, List, Tuple
+
+def dictionary() -> None:
+    dic: Dict = dict(x=10, y=11) # {'x': 10, 'y': 11}
+    dic = dict()
+    dic['a'] = 1
+    dic['b'] = 2
+
+    # setdefault(key[, default])
+    # If key is in the dictionary, return its value. If not, insert key with
+    # a value of default and return default. default defaults to None.
+    print(dic.setdefault('c', 3))
+    dic.pop('a')
+    key, value = dic.popitem() # Changed in v3.7 : LIFO order is now guaranteed
+    print(f'key = {key}, value = {value}')
+
+def default_dictionary() -> None:
+    dic: DefaultDict[str, int] = collections.defaultdict(int)
+    dic['A'] = 90
+    dic['B'] = 80
+    dic['C'] += 1
+    print(dic)
+
+def compound_dictionary() -> None:
+    colors: Tuple[str, int] =  [('white', 1), ('blue', 2), ('white', 3), ('blue', 4), ('red', 1)]
+    lst: List[Tuple] = colors
+    dic: DefaultDict[str, List[int]] = collections.defaultdict(list)
+
+    for key, value  in lst:
+        # dic[key] = value
+        # When keys are encountered again, the look-up proceeds normally(
+        # returning the list for that key) and the list.append() adds another
+        # value to the list.
+        dic[key].append(value)
+
+    # defaultdict(<class 'list'>, {'white': [1, 3], 'blue': [2, 4], 'red': [1]})
+    print(sorted(dic.items(), reverse=False))
+    # [('blue', [2, 4]), ('red', [1]), ('white', [1, 3])]
